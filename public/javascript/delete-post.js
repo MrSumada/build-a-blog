6770d3e02e5920a2ddc796201a17cab0ea136b5d
MrSumada <ricksamericana.com>
2022-03-22 00:06:02 -0400

    add javascript files

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,19 @@
+async function deleteHandler(event) {
+    event.preventDefault();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch('/api/posts', {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', deleteHandler);
